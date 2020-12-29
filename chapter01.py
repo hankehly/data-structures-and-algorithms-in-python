3caa34a27acaca3d4011015e2483c3751820b205
Hank Ehly <henry.ehly@gmail.com>
2020-12-29 15:11:24 +0900

    Finish chapter 1 creativity exercises

--- chapter01.py
+++ chapter01.py
@@ -194,7 +194,194 @@ def C_1_19():
 
 
 def C_1_20():
-    pass
+    import random
+
+    def shuffle_v1(data):
+        """
+        This was my first thought. This implementation keeps track of selected indices
+        and explicitly chooses unused indices for selection in subsequent iterations.
+        """
+        n = len(data)
+        indices_used = set()
+        sample_index = None
+        new_sequence = []
+        for i in range(n):
+            while sample_index is None or sample_index in indices_used:
+                sample_index = random.randint(0, n - 1)
+            indices_used.add(sample_index)
+            new_sequence.append(data[sample_index])
+        return new_sequence
+
+    def shuffle_v2(data):
+        """
+        This implementation is what the author is probably suggesting. It's much more
+        concise and mutates the input just like random.shuffle
+
+        Procedure:
+        1. Iterate over data indices (range(len(data)))
+        2. At index i, select a random index j from data (random int between 0, n)
+        3. Swap data[i] with data[j]
+        4. Repeat steps 2, 3 until iteration is complete
+
+        """
+        n = len(data)
+        for i in range(n):
+            j = random.randint(i, n - 1)
+            data[i], data[j] = data[j], data[i]
+        return data
+
+    # uncomment to check output
+    # print(shuffle_v2([1, 2, 3, 4, 5]))
+
+
+def C_1_21():
+    def f():
+        lines = []
+        while True:
+            try:
+                text = input("Enter something, or type ctrl-D to continue: ")
+                lines.append(text)
+            except EOFError:
+                break
+        n = len(lines)
+        # reverse the lines
+        for i in range(n // 2):
+            j = n - 1 - i
+            lines[i], lines[j] = lines[j], lines[i]
+        return lines
+
+    # print(f())
+
+
+def C_1_22():
+    def dot_product(a, b):
+        """
+        The definition of dot product I found says to compute the sum of the products
+        of corresponding pairs of values in 2 equal-length sequences; but the exercise
+        says to return an array c of length n.
+
+        To compute based on the above definition, just take the sum of c.
+        """
+        assert len(a) == len(b), "a and b must be the same length"
+        n = len(a)
+        c = [a[i] * b[i] for i in range(n)]
+        return c
+
+    act = dot_product([2, 4, 6], [3, 6, 9])
+    exp = [6, 24, 54]
+    assert act == exp
+
+
+def C_1_23():
+    def f():
+        a = [1, 2, 3]
+        try:
+            a[99] = 4
+        except IndexError:
+            print("Don't try buffer overflow attacks in Python")
+
+    # f()
+
+
+def C_1_24():
+    def compute_number_of_vowels(string):
+        count = 0
+        for char in string:
+            if char in "aeiou":
+                count += 1
+        return count
+
+    assert compute_number_of_vowels("hello world") == 3
+
+
+def C_1_25():
+    def remove_punctuation(s):
+        upper = map(chr, range(ord("A"), ord("Z") + 1))
+        lower = map(chr, range(ord("a"), ord("z") + 1))
+        extra = {" "}
+        valid = set(upper) | set(lower) | extra
+        cp = ""
+        for char in s:
+            if char in valid:
+                cp += char
+        return cp
+
+    assert remove_punctuation("Let's try, Mike.") == "Lets try Mike"
+
+
+def C_1_26():
+    def f(a, b, c):
+        """
+        The exercise hint says to try a case analysis for each pair of integers and an
+        operator, so I check the distributive property.
+
+        If we just want an int check, we could use isinstance or int constructor.
+        """
+        try:
+            a, b, c = int(a), int(b), int(c)
+            assert a * (b + c) == a * b + a * c
+            return True
+        except (TypeError, ValueError):
+            return False
+
+    def run():
+        resp = input("Enter three numbers, separated by a space: ")
+        args = resp.split(" ")
+        validity = "valid" if f(*args) else "invalid"
+        print(f"The values entered are {validity}")
+
+    # run()
+
+
+def C_1_27():
+    def factors_v1(n):
+        """
+        This was my first thought – buffer the larger values and replay them later.
+        """
+        k = 1
+        buf = []
+        while k * k < n:
+            if n % k == 0:
+                yield k
+                buf.insert(0, n // k)
+            k += 1
+        if k * k == n:
+            yield k
+        for val in buf:
+            yield val
+
+    def factors_v2(n):
+        """
+        This was another solution suggesting in the exercise hints. We start from the
+        square root and check if n is divisible by i.
+        """
+        k = 1
+        while k * k < n:
+            if n % k == 0:
+                yield k
+            k += 1
+        # at this point, k equals the square root of n
+        for i in range(k, 0, -1):
+            if n % i == 0:
+                yield n // i
+
+    n_ = 1000
+
+    v1 = [x for x in factors_v1(n_)]
+    v2 = [x for x in factors_v2(n_)]
+
+    assert v1 == v2
+
+
+def C_1_28():
+    def norm(v, p=2):
+        s = 0
+        for n in v:
+            s += n ** p
+        return s ** 0.5
+
+    assert norm((4, 3)) == 5
+    assert norm((4, 3), p=3) == (4 ** 3 + 3 ** 3) ** 0.5
 
 
 if __name__ == "__main__":
@@ -217,3 +404,12 @@ if __name__ == "__main__":
     C_1_17()
     C_1_18()
     C_1_19()
+    C_1_20()
+    C_1_21()
+    C_1_22()
+    C_1_23()
+    C_1_24()
+    C_1_25()
+    C_1_26()
+    C_1_27()
+    C_1_28()
