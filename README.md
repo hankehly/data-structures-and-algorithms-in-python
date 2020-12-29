716ec3189335193d95c5e9acb82744544941d098
Hank Ehly <henry.ehly@gmail.com>
2020-12-29 13:00:18 +0900

    Remove book excerpts from source code

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # data-structures-and-algorithms-in-python
 
-Exercise answers to [Data Structures and Algorithms in Python](https://www.amazon.co.jp/Structures-Algorithms-Python-Michael-Goodrich/dp/1118290275).
+Exercise answers to Data Structures and Algorithms in Python.
+
+- [Book link](https://www.amazon.co.jp/Structures-Algorithms-Python-Michael-Goodrich/dp/1118290275)
+- [Exercise hints](http://bcs.wiley.com/he-bcs/Books?action=resource&bcsId=8029&itemId=1118290275&resourceId=35782)
--- chapter01.py
+++ chapter01.py
@@ -1,12 +1,4 @@
 def R_1_01():
-    """
-    R-1.1
-    -----
-    Write a short Python function, is_multiple(n, m), that takes two integer values and
-    returns True if n is a multiple of m, that is, n = mi for some integer i, and False
-    otherwise.
-    """
-
     def is_multiple(n, m):
         return n % m == 0
 
@@ -15,14 +7,6 @@ def R_1_01():
 
 
 def R_1_02():
-    """
-    R-1.2
-    -----
-    Write a short Python function, is_even(k), that takes an integer value and returns
-    True if k is even, and False otherwise. However, your function cannot use the
-    multiplication, modulo, or division operators.
-    """
-
     def is_even(k):
         _, rem = divmod(k, 2)
         return rem == 0
@@ -32,15 +16,6 @@ def R_1_02():
 
 
 def R_1_03():
-    """
-    R-1.3
-    -----
-    Write a short Python function, minmax(data), that takes a sequence of one or more
-    numbers, and returns the smallest and largest numbers, in the form of a tuple of
-    length two. Do not use the built-in functions min or max in implementing your
-    solution.
-    """
-
     def minmax(data):
         _min = _max = data[0]
         for n in data:
@@ -58,13 +33,6 @@ def R_1_03():
 
 
 def R_1_04():
-    """
-    R-1.4
-    -----
-    Write a short Python function that takes a positive integer n and returns the sum of
-    the squares of all the positive integers smaller than n.
-    """
-
     def f(n):
         assert n > 0, "input value must be greater than zero"
         return sum([i ** 2 for i in range(n)])
@@ -74,23 +42,10 @@ def R_1_04():
 
 
 def R_1_05():
-    """
-    R-1.5
-    -----
-    Give a single command that computes the sum from Exercise R-1.4, relying on Python's
-    comprehension syntax and the built-in sum function.
-    """
     pass  # completed in R-1.4
 
 
 def R_1_06():
-    """
-    R-1.6
-    -----
-    Write a short Python function that takes a positive integer n and returns the sum of
-    the squares of all the odd positive integers smaller than n.
-    """
-
     def f(n):
         assert isinstance(n, int) and n > 0, "input value must be a positive integer"
         return sum([i ** 2 for i in range(1, n, 2)])
@@ -100,23 +55,10 @@ def R_1_06():
 
 
 def R_1_07():
-    """
-    R-1.7
-    -----
-    Give a single command that computes the sum from Exercise R-1.6, relying on Python's
-    comprehension syntax and the built-in sum function.
-    """
     pass  # completed in R-1.6
 
 
 def R_1_08():
-    """
-    R-1.8
-    -----
-    Python allows negative integers to be used as indices into a sequence, such as a
-    string. If string s has length n, and expression s[k] is used for index −n ≤ k < 0,
-    what is the equivalent index j ≥ 0 such that s[j] references the same element?
-    """
     s = "telephone"
     k = -5
     # to get the equivalent value of s[k] using j ≥ 0
@@ -128,46 +70,19 @@ def R_1_08():
 
 
 def R_1_09():
-    """
-    R-1.9
-    -----
-    What parameters should be sent to the range constructor, to produce a range with
-    values 50, 60, 70, 80?
-    """
     assert list(range(50, 80 + 1, 10)) == [50, 60, 70, 80]
 
 
 def R_1_10():
-    """
-    R-1.10
-    ------
-    What parameters should be sent to the range constructor, to produce a range with
-    values 8, 6, 4, 2, 0, −2, −4, −6, −8?
-    """
     assert list(range(8, -8 - 1, -2)) == [8, 6, 4, 2, 0, -2, -4, -6, -8]
 
 
 def R_1_11():
-    """
-    R-1.11
-    ------
-    Demonstrate how to use Python's list comprehension syntax to produce the list
-    [1, 2, 4, 8, 16, 32, 64, 128, 256].
-    """
     exp = [1, 2, 4, 8, 16, 32, 64, 128, 256]
     assert [2 ** i for i in range(len(exp))] == exp
 
 
 def R_1_12():
-    """
-    R-1.12
-    ------
-    Python's random module includes a function choice(data) that returns a random
-    element from a non-empty sequence. The random module includes a more basic function
-    randrange, with parameterization similar to the built-in range function, that
-    returns a random choice from the given range. Using only the randrange function,
-    implement your own version of the choice function.
-    """
     import random
 
     def choice(data):
@@ -181,20 +96,104 @@ def R_1_12():
     exp = [82, 9, 36, 11, 14, 18, 90, 17, 35, 54]
     choices = set()
 
-    for _ in range(50):
+    for _ in range(100):
         choices.add(choice(exp))
 
     assert choices == set(exp), f"\n\texp: {set(exp)}\n\tact: {choices}"
 
 
 def C_1_13():
-    """
-    C-1.13
-    ------
-    Write a pseudo-code description of a function that reverses a list of n integers, so
-    that the numbers are listed in the opposite order than they were before, and compare
-    this method to an equivalent Python function for doing the same thing.
-    """
+    def reverse(seq):
+        n = len(seq)
+        a = []
+        for i, k in enumerate(range(n - 1, -1, -1)):
+            a.append(seq[k])
+        return a
+
+    exp = [5, 4, 3, 2, 1]
+    act = reverse([1, 2, 3, 4, 5])
+
+    assert list(reversed([1, 2, 3, 4, 5])) == exp
+    assert act == exp, f"\n\tact: {act}\n\texp: {exp}"
+
+
+def C_1_14():
+    def f(seq):
+        for i in seq:
+            for j in seq:
+                if i == j:
+                    continue
+                if i * j % 2 != 0:
+                    return True
+        return False
+
+    assert f([3, 6, 8, 10]) is False
+    assert f([3, 6, 7, 10]) is True
+
+
+def C_1_15():
+    def distinct(seq):
+        seen = set()
+        for n in seq:
+            if n in seen:
+                return False
+            seen.add(n)
+        return True
+
+    assert distinct([1, 2, 3, 4, 5])
+    assert distinct([1, 2, 2, 3, 4]) is False
+
+
+def C_1_16():
+    # Scale function from page 25
+    def scale(data, factor):
+        for j in range(len(data)):
+            data[j] *= factor
+
+    seq = [1, 2, 3, 4, 5]
+    scale(seq, 2)
+
+    # int objects are immutable, but the list that contains them is mutable.
+    # The scale functions mutates the list by replacing each item with a new
+    # instance of int.
+    assert seq == [2, 4, 6, 8, 10]
+
+
+def C_1_17():
+    def scale(data, factor):
+        for val in data:
+            val *= factor
+
+    # No, data will stay the same. In the updated scale function, val is assigned to a
+    # new int object on each iteration; but this new value is never inserted into data
+    # ie. data is never mutated.
+    seq = [1, 2, 3, 4, 5]
+    scale(seq, 2)
+    assert seq == [1, 2, 3, 4, 5]
+
+
+def C_1_18():
+    exp = [0, 2, 6, 12, 20, 30, 42, 56, 72, 90]
+    # the factors of each number are (n, n-1)
+    # 1 * 0, 2 * 1, 3 * 2, ...
+    act = [n * (n - 1) for n in range(1, 11)]
+    assert act == exp
+
+
+def C_1_19():
+    exp = [
+        # fmt: off
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+        # fmt: on
+    ]
+    start = ord("a")
+    stop = ord("z") + 1
+    act = [chr(n) for n in range(start, stop)]
+    assert act == exp
+
+
+def C_1_20():
     pass
 
 
@@ -212,3 +211,9 @@ if __name__ == "__main__":
     R_1_11()
     R_1_12()
     C_1_13()
+    C_1_14()
+    C_1_15()
+    C_1_16()
+    C_1_17()
+    C_1_18()
+    C_1_19()
